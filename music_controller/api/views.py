3ee9a03b708da93bcd888923eaccf15839cb0d35
perezpefaur <perezpefaur@uc.cl>
2022-04-29 23:21:58 -0400

    urls api

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -5,10 +5,6 @@ from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-
-# Create your views here.
-
-
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
